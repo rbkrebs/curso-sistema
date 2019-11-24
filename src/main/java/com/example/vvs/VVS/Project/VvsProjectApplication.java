caf0223c2a893451cac4523444623126352d0624
romulo krebs <romulo.krebs@gmail.com>
2019-11-24 09:42:38 -0300

    Fisrt commit

---
+++ src/main/java/com/example/vvs/VVS/Project/VvsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vvs.VVS.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VvsProjectApplication.class, args);
+	}
+
+}
