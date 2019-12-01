da7b86997b7a2a4dc2037768badcb87968e86f57
romulo krebs <romulo.krebs@gmail.com>
2019-12-01 10:53:10 -0300

    ajuste para postgresql do heroku

--- src/main/java/com/example/vvs/VVS/Project/DataConfiguration.java
+++ src/main/java/com/example/vvs/VVS/Project/DataConfiguration.java
@@ -1,35 +1,33 @@
 package com.example.vvs.VVS.Project;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import java.net.URI;
 
 import javax.sql.DataSource;
+import java.net.URISyntaxException;
 
 @Configuration
 public class DataConfiguration {
 
     @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUsername("romulo");
-        dataSource.setPassword("romulo");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/romulo");
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        return dataSource;
-    }
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.POSTGRESQL);
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
-        adapter.setPrepareConnection(true);
-        return adapter;
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 }
